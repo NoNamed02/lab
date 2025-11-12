136e392208e1e6b84431201a6fcbefb482e2793b
Yukari <knite7989@gmail.com>
2025-11-12 10:08:44 +0900

    2025 11 12

---
+++ lab2_init/source/Source.cpp
@@ -0,0 +1,59 @@
+
+//#define GLFW_INCLUDE_GLU
+
+#include <iostream>
+
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+
+#include "MyGlWindow.h"
+
+int main(void)
+{
+	/* Initialize the library */
+	if (!glfwInit())
+	{
+	// Initialization failed
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	int width = 800;
+	int height = 800;
+	/* Create a windowed mode window and its OpenGL context */
+	GLFWwindow* window= glfwCreateWindow(width, height, "OpenGL FrameWork", NULL,
+	NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (gl3wInit())
+	{
+		fprintf(stderr, "failed to initialize OpenGL\n");
+		return -1;
+	}
+	if (!gl3wIsSupported(3, 2))
+	{
+		fprintf(stderr, "OpenGL 3.2 not supported\n");
+		return -1;
+	}
+	printf("OpenGL %s, GLSL %s\n", glGetString(GL_VERSION),
+	glGetString(GL_SHADING_LANGUAGE_VERSION));
+	glfwSwapInterval(1); //enable vsync
+	printf("OpenGL %s, GLSL %s\n",
+	glGetString(GL_VERSION),glGetString(GL_SHADING_LANGUAGE_VERSION));
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();	
+	return 0;
+}
+
+
